a8efeaf3ab872bc7ad1a96cf7aa6f73a6a9a5ee7
cwojtak <connor.wojtak@gmail.com>
2025-02-12 15:21:25 -0600

    Add extendedTypes and a couple of class modifiers

--- src/main/java/edu/university/ecs/lab/common/models/ir/JRecord.java
+++ src/main/java/edu/university/ecs/lab/common/models/ir/JRecord.java
@@ -4,6 +4,7 @@ import edu.university.ecs.lab.common.models.enums.AccessModifier;
 import edu.university.ecs.lab.common.models.enums.ClassRole;
 import edu.university.ecs.lab.common.models.enums.FileType;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -13,8 +14,8 @@ public class JRecord extends JClass {
         this.fileType = FileType.JRECORD;
     }
 
-    public JRecord(String name, String path, String packageName, ClassRole classRole, Set<Import> imports, Set<Method> methods, Set<Field> fields, Set<Annotation> classAnnotations, List<MethodCall> methodCalls, Set<String> implementedTypes, AccessModifier protection, Boolean isFinal) {
-        super(name, path, packageName, classRole, imports, methods, fields, classAnnotations, methodCalls, implementedTypes, protection, isFinal);
+    public JRecord(String name, String path, String packageName, ClassRole classRole, Set<Import> imports, Set<Method> methods, Set<Field> fields, Set<Annotation> classAnnotations, List<MethodCall> methodCalls, Set<String> implementedTypes, AccessModifier protection, Boolean isStatic) {
+        super(name, path, packageName, classRole, imports, methods, fields, classAnnotations, methodCalls, implementedTypes, new HashSet<String>(), protection, true, false, isStatic);
         this.fileType = FileType.JRECORD;
     }
 }
