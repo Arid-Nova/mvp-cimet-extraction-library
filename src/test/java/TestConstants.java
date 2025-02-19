522dee3760b45f7cc1808fca39d433cf786800d6
cwojtak <connor.wojtak@gmail.com>
2025-02-19 15:45:20 -0600

    Add another test from cimet2

---
+++ src/test/java/TestConstants.java
@@ -0,0 +1,7 @@
+import java.io.File;
+
+public class TestConstants {
+    public static final String RESOURCES_PATH = "src" + File.separator + "test" + File.separator + "resources";
+    public static final String CONFIGS_PATH = RESOURCES_PATH + File.separator + "configs";
+    public static final String JAVA_FILES_PATH = RESOURCES_PATH + File.separator + "java_files";
+}
