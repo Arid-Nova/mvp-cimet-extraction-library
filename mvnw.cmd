8d4d246a1ec3d2afd5d75045bc4eca076875aea1
Jose Sosa <josesosa@concucolabs.com>
2026-07-22 15:47:55 -0400

    Ensure -Dmaven.multiModuleProjectDirectory passed to wrapper JVM invocation

--- mvnw.cmd
+++ mvnw.cmd
@@ -20,4 +20,4 @@ if not defined JAVA_TOOL_OPTIONS (
   set JAVA_TOOL_OPTIONS=%JAVA_TOOL_OPTIONS% --enable-native-access=ALL-UNNAMED
 )
 
-java -cp "%WRAPPER_JAR%" org.apache.maven.wrapper.MavenWrapperMain %*
+java -Dmaven.multiModuleProjectDirectory=%CD% -cp "%WRAPPER_JAR%" org.apache.maven.wrapper.MavenWrapperMain %*
